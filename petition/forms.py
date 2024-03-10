7c1e80535d4d6cb4130f339c523b23d639614b76
Nicolas-CM <nicolascuellar.molina@gmail.com>
2024-03-10 15:05:17 -0500

    feat: crear formulario solicitud de monitoria

--- petition/forms.py
+++ petition/forms.py
@@ -2,4 +2,7 @@ from .models import Petition,Other,Monitoring
 from django import forms
 
 #class CreateNewPetition(forms.Form):
-    
+class CreateNewPetition(forms.ModelForm):
+    class Meta:
+        model = Monitoring
+        fields = '__all__'
