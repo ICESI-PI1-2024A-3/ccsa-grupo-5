e8cbbe1c63c8bd61cd3bfa3c09c18eacd599d3e4
Miguel-23-ing <miguelangelmartines148@gmail.com>
2024-03-18 15:37:55 -0500

    feat: agregar funcionalidad restablecer contraseña

--- moduloContratacion/settings.py
+++ moduloContratacion/settings.py
@@ -132,3 +132,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'login.User'
 
 FAVICON = os.path.join(BASE_DIR, 'favicon.ico')
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "modulocontratacion@gmail.com"
+EMAIL_HOST_PASSWORD = "bckeiknnfopwlnih"
