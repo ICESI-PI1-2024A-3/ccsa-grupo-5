17bb3e8f6f508dd90be0820e95479fd42fec69b3
davidone007 <davidecazaran@gmail.com>
2024-03-29 18:30:37 -0500

    fix: corregir vistas

--- petition/forms/createNewMonitoringPetition.py
+++ petition/forms/createNewMonitoringPetition.py
@@ -1,11 +1,20 @@
 from ..models import *
 from django import forms
 
+
 class CreateNewMonitoringPetition(forms.ModelForm):
+    statesChoices = [
+        ("pendiente", "Pendiente"),
+        ("en_proceso", "En Proceso"),
+    ]
+
+    # Filtrar las opciones para el campo 'state' en el formulario de la petición
+    state = forms.ChoiceField(choices=statesChoices, label="Estado")
+
     class Meta:
         model = Monitoring
-        fields = '__all__'
-        
+        fields = "__all__"
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['user'].required = False
+        self.fields["user"].required = False
--- petition/forms/createNewOtherPetition.py
+++ petition/forms/createNewOtherPetition.py
@@ -1,16 +1,25 @@
 from ..models import *
 from django import forms
 
+
 class CreateNewOtherPetition(forms.ModelForm):
+    statesChoices = [
+        ("pendiente", "Pendiente"),
+        ("en_proceso", "En Proceso"),
+    ]
+
+    # Filtrar las opciones para el campo 'state' en el formulario de la petición
+    state = forms.ChoiceField(choices=statesChoices, label="Estado")
+
     class Meta:
         model = Other
-        fields = '__all__'
-        
+        fields = "__all__"
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['user'].required = False
-    
+        self.fields["user"].required = False
+
     def clean_archivo(self):
-        archivo = self.cleaned_data.get('archivo')
+        archivo = self.cleaned_data.get("archivo")
         # Aquí puedes realizar validaciones adicionales si es necesario
         return archivo
