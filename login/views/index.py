a15a44cfb94a3f825a4ac96b554c5d286867d14e
davidone007 <davidecazaran@gmail.com>
2024-03-29 14:43:33 -0500

    feat: manejar errores login

--- login/views/index.py
+++ login/views/index.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm 
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 import login
-from login.permissions import create_groups
 from ..forms import loginForm, userForm
 from django.contrib import messages
 from django.contrib.auth import logout
@@ -14,8 +13,7 @@ from django.views.decorators.cache import never_cache
 from django.contrib.auth.models import User, Group
 
 
-
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
--- login/views/logoutSesion.py
+++ login/views/logoutSesion.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm 
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
@@ -6,22 +6,14 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 import login
-from login.permissions import create_groups
 from ..forms import loginForm, userForm
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.views.decorators.cache import never_cache
 from django.contrib.auth.models import User, Group
 
-        
-@login_required        
+
+@login_required
 def logoutSesion(request):
     logout(request)
-    return redirect('login')  
-
-        
-
-        
-
-
-
+    return redirect("login")
