77f4e172a1bf32441d0ddf69d683445be27e7ddb
davidone007 <davidecazaran@gmail.com>
2024-04-20 20:25:28 -0500

    fix: seguir probando

--- moduloContratacion/settings.py
+++ moduloContratacion/settings.py
@@ -29,7 +29,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG','False').lower() == 'false'
+DEBUG = os.environ.get('DEBUG','False').lower() == 'true'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
