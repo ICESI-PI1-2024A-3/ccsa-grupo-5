1809bb055716faf43508eddb42e5535013ba8191
Nicolas-CM <nicolascuellar.molina@gmail.com>
2024-05-20 10:00:55 -0500

    fix: corregir debug

--- moduloContratacion/settings.py
+++ moduloContratacion/settings.py
@@ -34,8 +34,8 @@ SECRET_KEY = "cdabadc5caf8207c81af4cf9404b6b33"
 if "test" in sys.argv:
     DEBUG = True
 else:
-    #DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-    DEBUG = True
+    DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+    #DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
