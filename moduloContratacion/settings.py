b5299c744003a2f400ec36da4040dddea428dbd3
Nicolas Cuellar <90799216+Nicolas-CM@users.noreply.github.com>
2024-05-20 14:33:14 -0500

    fix: arreglar base de datos

--- moduloContratacion/settings.py
+++ moduloContratacion/settings.py
@@ -90,14 +90,7 @@ WSGI_APPLICATION = "moduloContratacion.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db_test.sqlite3",
-        }
-    }
-
-else:
+    
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
